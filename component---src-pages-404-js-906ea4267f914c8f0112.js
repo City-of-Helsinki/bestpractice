6479cd99a0feeb37b9f2c1da46b1ca11e0116c73
petterill <petterill@users.noreply.github.com>
2020-06-08 11:48:21 +0000

    deploy: ae21d9d98e8db9771a377b855c974b8b83c1ee7a

---
+++ component---src-pages-404-js-906ea4267f914c8f0112.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[53],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-906ea4267f914c8f0112.js.map
